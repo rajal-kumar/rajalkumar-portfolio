75479719e94a03b5bfa2adf1e689aedefda61f10
Rajal Kumar <rajalkumar13@hotmail.com>
2018-11-08 13:54:07 +1300

    elements added to landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import '../App.css'
+import { Grid, Cell } from 'react-mdl'
 
 class LandingPage extends Component {
   render() {
     return (
-      <div className="page-LandingPage row">
-        
+      <div style={{width: '100%', margin: 'auto'}}>
+        <Grid className="landing-grid">
+            <Cell col={12}></Cell>
+        </Grid>
       </div>
     );
   }
