8710494403d454754bf5811edf0af4e32a4a9928
Rajal Kumar <rajalkumar13@hotmail.com>
2019-02-07 14:24:43 +1300

    experience component populated

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,8 +5,13 @@ class Experience extends Component {
   render() {
     return(
       <Grid>
-        <Cell col={4}>Left Side</Cell>
-        <Cell col={8}>Right Side</Cell>
+        <Cell col={4}>
+         <p>{this.props.startYear} - {this.props.endYear}</p>
+        </Cell>
+        <Cell col={8}>
+         <h4 style={{marginTop: '0px'}}>{this.props.jobName}</h4>
+         <p>{this.props.jobDescription}</p>
+        </Cell>
       </Grid>
     )
   }
