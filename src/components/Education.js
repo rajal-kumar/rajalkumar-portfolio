eff038b4988b2ce83df0ca8c0ab97ff2b6e08d18
Rajal Kumar <rajalkumar13@hotmail.com>
2019-02-07 14:08:55 +1300

    defining props

--- src/components/Education.js
+++ src/components/Education.js
@@ -5,7 +5,13 @@ class Education extends Component {
   render() {
     return(
       <Grid>
-        <Cell col={}></Cell>
+        <Cell col={4}>
+         <p>{this.props.startYear} - {this.props.endYear}</p>
+        </Cell>
+        <Cell col={8}>
+         <h4 style={{marginTop: '0px'}}>{this.props.schoolName}</h4>
+         <p>{this.props.schoolDescription}</p>
+        </Cell>
       </Grid>
     )
   }
