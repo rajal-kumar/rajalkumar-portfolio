2338aea168f61cb96ed3a0277c98bdacbce435ba
Rajal Kumar <rajalkumar13@hotmail.com>
2019-03-13 16:12:26 +1300

    app build complete ready to go up on to the web

---
+++ build/precache-manifest.aaa8a78af01ffa8da3936ed524ea21ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e97e2daebe7de01ad88",
+    "url": "/static/css/main.a18c2a22.chunk.css"
+  },
+  {
+    "revision": "4e97e2daebe7de01ad88",
+    "url": "/static/js/main.4e97e2da.chunk.js"
+  },
+  {
+    "revision": "fe708b94526b0b426a22",
+    "url": "/static/css/1.17b94ea8.chunk.css"
+  },
+  {
+    "revision": "fe708b94526b0b426a22",
+    "url": "/static/js/1.fe708b94.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bef97ec2911f858885ef981d51e99712",
+    "url": "/index.html"
+  }
+];
